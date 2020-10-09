738eb720bcef182d8356528a7e2a5e0287cd871c
yinyanjun <179063828@qq.com>
2020-10-09 16:11:01 +0800

    init

---
+++ src/main/java/com/tianyisoft/showdoc/ShowDocApplication.java
@@ -0,0 +1,13 @@
+package com.tianyisoft.showdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowDocApplication.class, args);
+	}
+
+}
